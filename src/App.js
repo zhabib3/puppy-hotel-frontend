f530941cdeec95bdaaa9ece1e6d2edbbc0ea76d1
Zeeshan Habib <zhabib@ufl.edu>
2020-02-11 15:46:10 -0500

    Added base layout including nav, grid and asset pics for puppies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Grid,
+  Container,
+  Image,
+  Menu,
+  Segment,
+  Header
+} from "semantic-ui-react";
 
 function App() {
+
+  let activeItem = 'puppies';
+
+  const handleItemClick = () => {
+    console.log("Menu item clicked");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor: '#b9f6ca' }}>
+      {/* Nav */}
+      <Segment inverted style={{ borderRadius: 0 }}>
+        <Menu inverted pointing secondary>
+
+          {/* Header */}
+          <Header
+            textAlign="center"
+            as="h1"
+            style={{color: "#fff"}}
+          >
+          <Image circular src="/images/french poodle.png" /> Puppy Hotel
+          </Header>
+
+          <Menu.Menu position='right'>
+            <Menu.Item
+              name='Puppies'
+              active={activeItem === 'puppies'}
+              onClick={handleItemClick}
+            />
+            <Menu.Item
+              name='Rooms'
+              active={activeItem === 'rooms'}
+              onClick={handleItemClick}
+            />
+
+          </Menu.Menu>
+
+        </Menu>
+      </Segment>
+
+      {/* Body */}
+      <Grid padded columns={2} style={{ minHeight: '90vh', backgoundColor: '#ffcdd2' }}>
+        {/* List */}
+        <Grid.Column width={10}>
+          <Segment raised>
+            List
+          </Segment>
+
+        </Grid.Column>
+        {/* Edit Item */}
+        <Grid.Column width={6}>
+        <Segment raised>
+            Edit
+          </Segment>
+        </Grid.Column>
+      </Grid>
     </div>
   );
 }
